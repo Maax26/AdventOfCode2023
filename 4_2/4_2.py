dd33c0cc09de197916ce969500bf4eee50ac6d4d
Max Månsson <max.mansson@live.se>
2023-12-10 12:49:30 +0100

    Apply pylint 4_2

--- 4_2/4_2.py
+++ 4_2/4_2.py
@@ -2,13 +2,13 @@ from collections import defaultdict
 
 
 def main():
-    # inputs = open("test_input.txt")
-    inputs = open("input.txt")
+    # inputs = open("test_input.txt", encoding="utf-8")
+    inputs = open("input.txt", encoding="utf-8")
 
     card_instances = defaultdict(int)
     for i, card in enumerate(inputs):
         card_instances[i] += 1
-        card_str, numbers_str = card.strip().split(":")
+        numbers_str = card.strip().split(":")[1]
         winning_numbers_str, your_numbers_str = numbers_str.strip().split("|")
         winning_numbers = [w for w in winning_numbers_str.strip().split(" ") if w]
         your_numbers = [y for y in your_numbers_str.strip().split(" ") if y]
